b3fd7b4ba0dde8d103406eb5b981b4e9a14f3cc2
Tevel <119736325+Tadataka-Rei@users.noreply.github.com>
2026-04-02 19:06:39 +0700

    refactor(PlayerSkill): change board creation logic and direction calculation

--- scripts/Player_control/Playerskill.gd
+++ scripts/Player_control/Playerskill.gd
@@ -25,23 +25,34 @@ func _input(event: InputEvent) -> void:
 			board_created = not board_created
 			if board_created:
 				var direction_vector = calculate_direction_vector()
-				create_board(8, direction_vector)
+				create_board(12, direction_vector)
 			else:
 				destroy_board()
 #endregion
 
 #region ----------Caculate board XZ-------------------
 func calculate_direction_vector() -> Vector3:
-	var direction_vector: Vector3 = (global_position - target_enemy.global_position).normalized()
-	direction_vector.y = 0
-	angle = atan2(direction_vector.x, direction_vector.z)
-	return direction_vector
-#endregion 
+	if target_enemy == null:
+		return Vector3.FORWARD
+
+	var dir: Vector3 = (target_enemy.global_position - global_position)
+	dir.y = 0
+	dir = dir.normalized()
+
+	# Snap to closest axis (remove diagonals)
+	if abs(dir.x) > abs(dir.z):
+		dir = Vector3(sign(dir.x), 0, 0) # left/right
+	else:
+		dir = Vector3(0, 0, sign(dir.z)) # forward/back
+
+	angle = atan2(dir.x, dir.z)
+	return dir
 
 #region ----------- Caculate Y spawn POS ---------
-# this part is not yet done and full of error, not facing the right direction and not spawning at the right location z,y, need to be fixed later
-func Caculate_Y_Point() -> float:
-	return min(target_enemy.global_position.y, global_position.y) - lower_value;
+func calculate_spawn_y() -> float:
+	if target_enemy == null:
+		return global_position.y
+	return min(target_enemy.global_position.y, global_position.y) - lower_value
 #endregion
 
 #region ---------------- Board Creation----------------
@@ -49,26 +60,29 @@ func create_board(board_size: int, direction_to_enemy: Vector3) -> void:
 	total_instances = board_size * board_size
 	multimesh.instance_count = total_instances
 
-	var forward = direction_to_enemy
-	var right = Vector3.UP.cross(forward).normalized()
-	
-	var half_board = (board_size - 1) * spacing * 0.5
-	var starting_point = global_position - (forward * half_board) - (right * half_board)
-	
-	var final_y = Caculate_Y_Point()
-	var board_basis = Basis(right, Vector3.UP, -forward).orthonormalized()
+	var forward = direction_to_enemy.normalized()
+	var right = forward.cross(Vector3.UP).normalized()
+	var up = Vector3.UP
+
+	# Center on enemy
+	var world_center = target_enemy.global_position
+	world_center.y = 0
+
+	var half_extent = (board_size - 1) * spacing * 0.5
+	var starting_point = world_center - (right * half_extent) - (forward * half_extent)
+
+	var board_basis = Basis(right, up, forward).orthonormalized()
+
 	for i: int in range(total_instances):
 		var x = i % board_size
 		@warning_ignore("integer_division")
 		var z = i / board_size
-		
-		var local_offset = (forward * z * spacing) + (right * x * spacing)
-		var final_pos = starting_point + local_offset
-		final_pos.y = 2
-		
-		var xform = Transform3D(Basis(), final_pos)
+
+		var world_pos = starting_point + (right * x * spacing) + (forward * z * spacing)
+		var local_pos = to_local(world_pos)
+
+		var xform = Transform3D(Basis(), local_pos)
 		multimesh.set_instance_transform(i, xform)
-		print_debug(final_pos)
 
 		var color = Color.WHITE if (x + z) % 2 == 0 else Color.BLACK
 		multimesh.set_instance_color(i, color)
