0292f535348ca142a0d40a214bdd38c2d8b8276b
Tevel <119736325+Tadataka-Rei@users.noreply.github.com>
2026-04-28 13:04:06 +0700

    feat: add Float_updown shader and multimesh instance creation

---
+++ scripts/multimeshinstances/create_in_area.gd
@@ -0,0 +1,19 @@
+extends MultiMeshInstance3D
+
+@export var cube_count := 200
+@export var area_size := 100.0
+@export var min_height := 1.0
+@export var max_height := 10.0
+
+func _ready():
+	multimesh.instance_count = cube_count
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for i in cube_count:
+		var x = rng.randf_range(-area_size, area_size)
+		var z = rng.randf_range(-area_size, area_size)
+		var y = rng.randf_range(min_height, max_height)
+		
+		var transform = Transform3D()
+		transform.origin = Vector3(x, y, z)
+		multimesh.set_instance_transform(i, transform)
